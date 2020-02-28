3d4a65d51937ef798e088c8f807e0c5b02bab94c
Andrey Kudryavcev <andykey@bk.ru>
2020-02-28 08:05:21 +0700

    1

--- Lesson_4/1/1.js
+++ Lesson_4/1/1.js
@@ -1,1 +1,17 @@
 'use strict'
+
+/**
+ * Функция конвертирует число в объект
+ * @param {number} number Число от 0 до 999
+ * @returns {object}
+ */
+function numerToObject(number){
+    let numberObject = {};
+    if (number >= 0 && number <= 999){
+        return number;
+    } else {
+        return numberObject;
+    }
+}
+
+console.log(numerToObject(100));
