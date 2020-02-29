e97afafb62f1bf521e8c294a3896eb2d8f51daa2
Andrey Kudryavcev <andykey@bk.ru>
2020-02-29 14:47:51 +0700

    Выполнил задание 1 Начал задание 2

--- Lesson_4/1/1.js
+++ Lesson_4/1/1.js
@@ -1,17 +1,33 @@
 'use strict'
 
+function NumberObj (hundreds, tens, units){
+    this.units = units; //это единицы
+    this.tens = tens; //это десятки
+    this.hundreds = hundreds; //это сотни
+}
+
 /**
  * Функция конвертирует число в объект
  * @param {number} number Число от 0 до 999
  * @returns {object}
  */
-function numerToObject(number){
+function numberToObject(number){
     let numberObject = {};
+    if (!Number.isInteger(number)){
+        console.log("Введите целое число");
+        return numberObject;
+    }
+
     if (number >= 0 && number <= 999){
-        return number;
+        number = String(number);
+        while (number.length < 3) {
+            number = "0" + number;
+        }
+        let numberObject = new NumberObj (parseInt(number[0]), parseInt(number[1]), parseInt(number[2]));
+        return numberObject;
     } else {
         return numberObject;
     }
 }
 
-console.log(numerToObject(100));
+console.log(numberToObject(1000));
